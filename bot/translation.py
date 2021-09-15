57d232beff8a54c11eabb1d106998781cf056f91
CR7 <87358439+Akashvbf@users.noreply.github.com>
2021-09-15 11:40:51 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -6,29 +6,13 @@ class Translation(object):
     
     START_TEXT = """👋Hey {},
 
-I'm an Autofilter bot v2.0 and you can not Add Me to your Group. I was made for <a href="t.me/{}">this one group</a>. So don't waste your Time...\n\n{}"""
+ഈ ബോട്ട് moviemarket ഗ്രൂപ്പിലേക്ക് ഉള്ളത് എന്ന് ഇനി വീണ്ടും വീണ്ടും പറയണോ??
+അപ്പോ പിന്നെ എന്തിനാ വീണ്ടും വീണ്ടും സ്റ്റാർട്ട് കുത്തി കളിക്കാൻ വരുന്നേ... ആ സൈഡിലോട്ട് എങ്ങാനും മാറി ഇരിക്ക്‌ ഇനി🤭🤭
+@MOVIEMARKET_DISCUSSION_GROUP"""
 
 
     HELP_TEXT = """
-<b><u>Bot Commands (Works Only In Groups)</u></b>
-
-☞ <code>/add chat_id</code> - <b>To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)</b>
-   
-☞ <code>/del chat_id</code> - <b>To disconnect A Group With A Channel</b>
-     
-☞ <code>/delall</code>  - <b>This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB</b>
-    
-☞ <code>/settings</code> -  <b>This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly</b>
-
-   ☞ <code>Channel</code> - <b>Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls</b>
-            
-   ☞ <code>Filter Types</code> - <b>Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart</b>
-
-   ☞ <code>Configure</code> - <b>Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results</b>
-            
-   ☞ <code>Status</code> - <b>Button Will Shows The Stats Of Your Channel</b>
-        
-<b><a href="https://t.me/Mo_Tech_YT">© Mo Tech YT</a></b>
+Sorry, I can't help you
 """
     
     ABOUT_TEXT = """<b>☞ 📃Name</b> : <b>Pro Auto Filter Bot V2.O</b>
@@ -37,7 +21,7 @@ I'm an Autofilter bot v2.0 and you can not Add Me to your Group. I was made for 
 
 <b>☞ 📚Library</b> : Pyrogram Asyncio 1.13.0
 
-<b>☞ 📖Source Code</b> : <a href="https://github.com/MoTechYT/Pro-Auto-Filter-Bot-V2.o">😋Click Here</a></b>
+<b>☞ 📖Source Code</b> : <a href="https://telegra.ph/file/efa59bd0958efbf4f6b43.jpg">😋Click Here</a></b>
 """
 
-    TEAM = """Team [MoTech YT](t.me/Mo_Tech_YT)"""
+    TEAM = """Team [Movie Market](t.me/MOVIEMARKET_DISCUSSION_GROUP)"""
